657e56bfa3ce4322adf267062d66acc99fec0d52
AnamikaDeb25 <83755934+AnamikaDeb25@users.noreply.github.com>
2021-06-23 13:24:04 +0530

    Find essential services near me

---
+++ app/src/main/java/com/example/find/startpage.java
@@ -0,0 +1,15 @@
+package com.example.find;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startpage);
+
+    }
+}
